563d240e6f018d390e0781decf5bbce406fd20fd
Ilana Shukhman <Ilana77001@gmail.com>
2021-01-04 14:47:06 +0200

    Added option to take a photo for profile picture

--- app/src/main/java/com/example/myapplication/BottomNavigationDrawerFragment.java
+++ app/src/main/java/com/example/myapplication/BottomNavigationDrawerFragment.java
@@ -1,18 +1,22 @@
 package com.example.myapplication;
 
+import android.Manifest;
 import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.ContentResolver;
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.MimeTypeMap;
-import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.Continuation;
@@ -25,7 +29,6 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.StorageTask;
@@ -36,20 +39,22 @@ import androidx.annotation.Nullable;
 
 import java.util.HashMap;
 
+import static androidx.core.content.ContextCompat.checkSelfPermission;
+
+
 public class BottomNavigationDrawerFragment extends BottomSheetDialogFragment {
 
     private StorageReference storageReference;
-    private static final String TAG = "ProfileActivity";
     private static final int IMAGE_REQUEST = 1;
+    private static final int PERMISSION_CODE = 1000;
+    private static final int IMAGE_CAPTURE_CODE = 1001;
     private StorageTask<UploadTask.TaskSnapshot> uploadTask;
     private Context mContext;
     private DatabaseReference reference;
     private FirebaseUser fUser;
     private FirebaseAuth mAuth;
-    private FirebaseFirestore db;
 
     private Uri imageUri;
-    private ImageView image_profile;
 
     @Override
     public void onAttach(Context context) {
@@ -79,13 +84,18 @@ public class BottomNavigationDrawerFragment extends BottomSheetDialogFragment {
                 switch (menuItem.getItemId()){
                     case R.id.upload_image:
                         openImage();
-                        return true;
                     case R.id.delete_image:
                         reference.child("imageUrl").setValue("default");
-                        return true;
                     case R.id.open_camera:
-                        Toast.makeText(mContext, "Nav item Three is Clicked ", Toast.LENGTH_SHORT).show();
-                        return true;
+                        //ask the user for permissions
+                        if(mContext.checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED ||
+                                mContext.checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED){
+                            String[] permission = {Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE};
+                            requestPermissions(permission, PERMISSION_CODE);
+                        }
+                        else {
+                            openCamera();
+                        }
                 }
                 return false;
             }
@@ -140,7 +150,6 @@ public class BottomNavigationDrawerFragment extends BottomSheetDialogFragment {
                             Uri downloadUri = task.getResult();
                             String mUri = downloadUri.toString();
 
-                            fUser = mAuth.getCurrentUser();
                             HashMap<String, Object> map = new HashMap<>();
                             map.put("imageUrl", mUri);
                             reference.updateChildren(map);
@@ -167,21 +176,54 @@ public class BottomNavigationDrawerFragment extends BottomSheetDialogFragment {
         }
     }
 
+    private void openCamera() {
+        ContentValues values = new ContentValues();
+        values.put(MediaStore.Images.Media.TITLE, "New Photo");
+        values.put(MediaStore.Images.Media.DESCRIPTION, "From the camera");
+        imageUri = getActivity().getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+        //Camera intent
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+        startActivityForResult(cameraIntent, IMAGE_CAPTURE_CODE);
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == PERMISSION_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                openCamera();
+            } else {
+                Toast.makeText(mContext, "Permission denied", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+
     @Override
     public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if(data == null || data.getData() == null){
+        //Upload Image result
+        if(data == null || data.getData() != null){
             Toast.makeText(getContext(), "Opening image Failed.",Toast.LENGTH_SHORT).show();
         }
 
-        if(requestCode == IMAGE_REQUEST && resultCode == Activity.RESULT_OK){
+        else if(requestCode == IMAGE_REQUEST && resultCode == Activity.RESULT_OK){
             imageUri = data.getData();
 
-            if(uploadTask != null && uploadTask.isInProgress()){
-                Toast.makeText(getContext(), "Uploading In Progress",Toast.LENGTH_SHORT).show();
+            if (uploadTask != null && uploadTask.isInProgress()) {
+                Toast.makeText(getContext(), "Uploading In Progress", Toast.LENGTH_SHORT).show();
+            } else {
+                uploadImage();
             }
-            else{
+        }
+
+        //Camera result
+        if(requestCode == IMAGE_CAPTURE_CODE && resultCode == Activity.RESULT_OK){
+            if (uploadTask != null && uploadTask.isInProgress()) {
+                Toast.makeText(getContext(), "Uploading In Progress", Toast.LENGTH_SHORT).show();
+            } else {
                 uploadImage();
             }
         }
